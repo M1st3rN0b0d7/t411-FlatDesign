f50f46e15ef91458e0bbbd7f902c447b73415352
M1st3rN0b0d7 <m1st3r-n0b0d7@gmx.fr>
2016-11-11 19:13:15 +0100

    Update t411_FlatDesign.js

--- t411_FlatDesign.js
+++ t411_FlatDesign.js
@@ -6,16 +6,15 @@
 // @author       M1st3rN0b0d7
 // @match        https://www.t411.ch/*
 // @grant        none
-// @require      none
 // ==/UserScript==
 
-if (!document.getElementById("FlatDesign")){
-    var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    link.id   = "FlatDesign";
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'https://cdn.rawgit.com/M1st3rN0b0d7/t411-flat-design/master/style.css';
+if(!document.getElementById("FlatDesign")){
+    var head   = document.getElementsByTagName('head')[0];
+    var link   = document.createElement('link');
+    link.id    = "FlatDesign";
+    link.rel   = 'stylesheet';
+    link.type  = 'text/css';
+    link.href  = 'https://rawgit.com/M1st3rN0b0d7/t411-flat-design/master/style.css';
     link.media = 'all';
     head.appendChild(link);
 }
